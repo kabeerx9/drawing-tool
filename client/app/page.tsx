ebc5a403d2bd6bf6d1a69daad52bb18ab6e43efb
kabeerx9 <kabeer786joshi@gmail.com>
2024-09-15 23:12:50 +0530

    SOCKET INITIALIZATION CHECK NEEDED TO BE DONE , BEFORE RENDERING COMPONENTS THAT GET THE SOCKET..

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,74 +1,76 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
-	const [roomId, setRoomId] = useState('');
-	const [username, setUsername] = useState('');
-	const router = useRouter();
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  const router = useRouter();
 
-	useEffect(() => {
-		const storedUsername = localStorage.getItem('username');
-		if (storedUsername) {
-			setUsername(storedUsername);
-		}
-	}, []);
+  useEffect(() => {
+    const storedUsername = localStorage.getItem("username");
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
 
-	const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-		localStorage.setItem('username', username);
-		// join the room now :
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    localStorage.setItem("username", username);
+    // join the room now :
+    router.push(`/room/${roomId}`);
+  };
 
-		router.push(`/room/${roomId}`);
-	};
-
-	return (
-		<div className="w-full h-screen flex items-center justify-center bg-gradient-to-r from-gray-100 to-gray-200">
-			<div className="bg-white p-8 rounded-lg shadow-lg w-96 border border-gray-300">
-				<h1 className="text-3xl font-bold mb-6 text-center text-black">
-					Join a Room
-				</h1>
-				<form onSubmit={handleSubmit} className="space-y-6">
-					<div>
-						<label
-							htmlFor="roomId"
-							className="block text-sm font-medium text-black mb-1">
-							Room ID
-						</label>
-						<input
-							type="text"
-							id="roomId"
-							value={roomId}
-							onChange={(e) => setRoomId(e.target.value)}
-							className="mt-1 p-2 border-2 border-black block w-full rounded-md  shadow-sm  text-black"
-							required
-						/>
-					</div>
-					<div>
-						<label
-							htmlFor="username"
-							className="block text-sm font-medium text-black mb-1">
-							Username
-						</label>
-						<input
-							type="text"
-							id="username"
-							value={username}
-							onChange={(e) => setUsername(e.target.value)}
-							className="mt-1 p-2 border-2 border-black block w-full rounded-md  shadow-sm  text-black"
-							required
-						/>
-					</div>
-					<button
-						type="submit"
-						className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors duration-200">
-						Join Room
-					</button>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-gradient-to-r from-gray-100 to-gray-200">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96 border border-gray-300">
+        <h1 className="text-3xl font-bold mb-6 text-center text-black">
+          Join a Room
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label
+              htmlFor="roomId"
+              className="block text-sm font-medium text-black mb-1"
+            >
+              Room ID
+            </label>
+            <input
+              type="text"
+              id="roomId"
+              value={roomId}
+              onChange={(e) => setRoomId(e.target.value)}
+              className="mt-1 p-2 border-2 border-black block w-full rounded-md  shadow-sm  text-black"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-black mb-1"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="mt-1 p-2 border-2 border-black block w-full rounded-md  shadow-sm  text-black"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors duration-200"
+          >
+            Join Room
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- client/app/room/[roomId]/page.tsx
+++ client/app/room/[roomId]/page.tsx
@@ -4,22 +4,36 @@ import Board from "@/components/board/board";
 import Menu from "@/components/menu/menu";
 import Toolbox from "@/components/toolbox/toolbox";
 
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { initializeSocket, joinRoom } from "@/app/store/slices/socketSlice";
+import { Divide } from "lucide-react";
 
 const RoomPage = () => {
+  console.log("Room page render ");
   const dispatch = useAppDispatch();
   const router = useRouter();
   const { roomId } = useParams();
+
+  const [socketInitialized, setSocketInitialized] = useState(false);
 
   const { isConnected, connectedUsers } = useAppSelector(
     (state) => state.socket,
   );
 
-  useEffect(() => {
-    dispatch(initializeSocket());
+  useLayoutEffect(() => {
+    console.log("use Layout Effect ");
+    const cleanup = dispatch(initializeSocket());
+    setSocketInitialized(true);
+    return cleanup;
+  }, [dispatch]);
+
+  useLayoutEffect(() => {
+    console.log("use Layout Effect ");
+    const cleanup = dispatch(initializeSocket());
+
+    return cleanup;
   }, [dispatch]);
 
   useEffect(() => {
@@ -33,11 +47,19 @@ const RoomPage = () => {
     }
   }, [isConnected, roomId, dispatch, router]);
 
+  if (!socketInitialized)
+    return (
+      <div className="w-full h-full bg-green-800 text-white font-bold text-3xl flex items-center justify-center">
+        Initializing...
+      </div>
+    );
+
   return (
     <div className="w-full h-full flex flex-col items-center">
       <Menu />
       <div className="flex items-start w-full">
         <Toolbox />
+        Kabeer Kabeer
       </div>
       <Board />
     </div>
